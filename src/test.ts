e8be0b6aa821ed900667c1aa723bdc18a39db1c7
chenhaiyong1 <chenhaiyong1@gome.com.cn>
2025-02-20 17:21:14 +0800

    =test

---
+++ src/test.ts
@@ -0,0 +1,13 @@
+//帮我写一段斐波那契函数
+function fib(n: number): number {
+    if (n === 0) {
+        return 0; 
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fib(n - 1) + fib(n - 2);
+    }
+    if(n<0){
+}
+console.log(fib(10)); // 55
+console.log(fib(20)); // 6765
